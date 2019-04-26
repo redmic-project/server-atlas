82bfa238653f658a4b5428e60f7d8b37beb62126
Noel Alonso <noel@redmic.es>
2019-04-26 13:53:35 +0100

    Añade dto de ThemeInspire + tests

---
+++ atlas-lib/src/main/java/es/redmic/atlaslib/dto/themeinspire/ThemeInspireDTO.java
@@ -0,0 +1,135 @@
+package es.redmic.atlaslib.dto.themeinspire;
+
+/*-
+ * #%L
+ * atlas-lib
+ * %%
+ * Copyright (C) 2019 REDMIC Project / Server
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import es.redmic.brokerlib.avro.data.common.DomainDTO;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ThemeInspireDTO extends DomainDTO {
+
+	// @formatter:off
+
+	@JsonIgnore
+	public static final org.apache.avro.Schema SCHEMA$ = new org.apache.avro.Schema.Parser().parse(
+		"{\"type\":\"record\",\"name\":\"ThemeInspireDTO\",\"namespace\":\"es.redmic.atlaslib.dto.themeinspire\",\"fields\":["
+				+ "{\"name\":\"code\",\"type\":\"string\"},"
+				+ "{\"name\":\"name\",\"type\":[\"string\", \"null\"]},"
+				+ "{\"name\":\"name_en\",\"type\":[\"string\", \"null\"]},"
+				+ "{\"name\":\"id\",\"type\":\"string\"}]}");
+	// @formatter:on
+
+	@Size(min = 1, max = 50)
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	@JsonIgnore
+	@Override
+	public org.apache.avro.Schema getSchema() {
+		return SCHEMA$;
+	}
+
+	@JsonIgnore
+	@Override
+	public java.lang.Object get(int field$) {
+		switch (field$) {
+		case 0:
+			return code;
+		case 1:
+			return getName();
+		case 2:
+			return getName_en();
+		case 3:
+			return getId();
+		default:
+			throw new org.apache.avro.AvroRuntimeException("Bad index");
+		}
+	}
+
+	@JsonIgnore
+	@Override
+	public void put(int field$, java.lang.Object value$) {
+		switch (field$) {
+		case 0:
+			code = value$ != null ? value$.toString() : null;
+			break;
+		case 1:
+			setName(value$ != null ? value$.toString() : null);
+			break;
+		case 2:
+			setName_en(value$ != null ? value$.toString() : null);
+			break;
+		case 3:
+			setId(value$.toString());
+			break;
+		default:
+			throw new org.apache.avro.AvroRuntimeException("Bad index");
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
+		result = prime * result + ((getName_en() == null) ? 0 : getName_en().hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ThemeInspireDTO other = (ThemeInspireDTO) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		if (getName() == null) {
+			if (other.getName() != null)
+				return false;
+		} else if (!getName().equals(other.getName()))
+			return false;
+		if (getName_en() == null) {
+			if (other.getName_en() != null)
+				return false;
+		} else if (!getName_en().equals(other.getName_en()))
+			return false;
+		return true;
+	}
+}
---
+++ atlas-lib/src/test/java/es/redmic/atlaslib/unit/dto/themeinspire/ThemeInspireCheckDTOValidationTest.java
@@ -0,0 +1,88 @@
+package es.redmic.atlaslib.unit.dto.themeinspire;
+
+/*-
+ * #%L
+ * atlas-lib
+ * %%
+ * Copyright (C) 2019 REDMIC Project / Server
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.redmic.atlaslib.dto.themeinspire.ThemeInspireDTO;
+import es.redmic.testutils.dto.DTOBaseTest;
+
+public class ThemeInspireCheckDTOValidationTest extends DTOBaseTest<ThemeInspireDTO> {
+
+	private static ThemeInspireDTO dto;
+	
+	@Before
+	public void reset() {
+
+		dto = new ThemeInspireDTO();
+		dto.setId("1");
+		dto.setCode("1234");
+		dto.setName("type");
+		dto.setName_en("type");
+	}
+
+	@Test
+	public void validationDTO_NoReturnError_IfDTOIsCorrect() {
+
+		checkDTOHasNoError(dto);
+	}
+
+	@Test
+	public void validationDTO_ReturnNotNullError_IfNameIsNull() {
+
+		dto.setName(null);
+
+		checkDTOHasError(dto, NOT_NULL_MESSAGE_TEMPLATE);
+	}
+
+	@Test
+	public void validationDTO_ReturnSizeError_IfNameExceedsSize() {
+
+		dto.setName(generateString(151));
+
+		checkDTOHasError(dto, SIZE_MESSAGE_TEMPLATE);
+	}
+
+	@Test
+	public void validationDTO_ReturnNotNullError_IfName_enIsNull() {
+
+		dto.setName_en(null);
+
+		checkDTOHasError(dto, NOT_NULL_MESSAGE_TEMPLATE);
+	}
+
+	@Test
+	public void validationDTO_ReturnSizeError_IfName_enExceedsSize() {
+
+		dto.setName_en(generateString(151));
+
+		checkDTOHasError(dto, SIZE_MESSAGE_TEMPLATE);
+	}
+
+	@Test
+	public void validationDTO_ReturnSizeError_IfCodeExceedsSize() {
+
+		dto.setCode(generateString(51));
+
+		checkDTOHasError(dto, SIZE_MESSAGE_TEMPLATE);
+	}
+}
