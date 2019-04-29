d9b0d09125faa1c9a786ed8ee5577ff56eb394f0
Noel Alonso <noel@redmic.es>
2019-04-29 14:30:55 +0100

    Añade rutas de librerías donde escanear componente

--- atlas-commands/src/main/java/es/redmic/atlascommands/AtlasCommandsApplication.java
+++ atlas-commands/src/main/java/es/redmic/atlascommands/AtlasCommandsApplication.java
@@ -1,47 +1,41 @@
 package es.redmic.atlascommands;
 
-/*-
- * #%L
- * atlas commands
- * %%
- * Copyright (C) 2019 REDMIC Project / Server
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import es.redmic.commandslib.config.GenerateJsonSchemaScanBean;
+import es.redmic.restlib.config.ResourceBundleMessageSource;
 import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
-@ComponentScan({ "es.redmic.atlascommands" })
+@ComponentScan({ "es.redmic.atlascommands", "es.redmic.restlib", "es.redmic.commandslib", "es.redmic.brokerlib.alert" })
 public class AtlasCommandsApplication {
-	
+
 	@Value("${info.microservice.name}")
 	String microserviceName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtlasCommandsApplication.class, args);
 	}
-	
+
+	@Bean
+	public MessageSource messageSource() {
+
+		return new ResourceBundleMessageSource();
+	}
+
+	@Bean
+	public GenerateJsonSchemaScanBean generateSchemaScanBean() {
+		return new GenerateJsonSchemaScanBean();
+	}
+
 	@PostConstruct
 	@Bean
 	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
