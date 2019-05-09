d3fa35120db4a92a445ce0163e09a5926fd5a209
Noel Alonso <noel@redmic.es>
2019-05-09 13:47:13 +0100

    Añade dto + tests para layerInfo
    LayerInfo es la información que añade el operario y que complementa a la
    definida en la capa.

---
+++ atlas-lib/src/main/java/es/redmic/atlaslib/dto/layerinfo/LayerInfoDTO.java
@@ -0,0 +1,174 @@
+package es.redmic.atlaslib.dto.layerinfo;
+
+/*-
+ * #%L
+ * Atlas-lib
+ * %%
+ * Copyright (C) 2019 REDMIC Project / Server
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import javax.validation.Valid;
+
+import org.apache.avro.Schema;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaUrl;
+
+import es.redmic.atlaslib.dto.category.CategoryDTO;
+import es.redmic.atlaslib.dto.layer.LatLonBoundingBoxDTO;
+import es.redmic.atlaslib.dto.layer.LayerCompactDTO;
+import es.redmic.atlaslib.dto.layer.ProtocolDTO;
+import es.redmic.atlaslib.dto.themeinspire.ThemeInspireDTO;
+import es.redmic.brokerlib.deserializer.CustomRelationDeserializer;
+
+/**
+ * DTO de entrada para datos enviados por el cliente y que complementan a los
+ * obtenidos vía getCapability.
+ * 
+ * Necesario validación DTO
+ * 
+ */
+public class LayerInfoDTO extends LayerCompactDTO {
+
+	// @formatter:off
+
+	@JsonIgnore
+	public static final org.apache.avro.Schema SCHEMA$ = new org.apache.avro.Schema.Parser().parse(
+		"{\"type\":\"record\",\"name\":\"LayerDTO\",\"namespace\":\"es.redmic.atlaslib.dto.layerinfo\",\"fields\":["
+			+ "{\"name\":\"parent\",\"type\":[" + CategoryDTO.SCHEMA$ + ",\"null\"]},"
+			+ "{\"name\":\"themeInspire\",\"type\":["+ ThemeInspireDTO.SCHEMA$ +", \"null\"]},"
+			+ "{\"name\":\"latLonBoundsImage\",\"type\":[" + LatLonBoundingBoxDTO.SCHEMA$ + ", \"null\"]},"
+			+ "{\"name\": \"protocols\",\"type\": [{\"type\": \"array\",\"items\":" + ProtocolDTO.SCHEMA$ + "},\"null\"]},"
+			+ "{\"name\":\"description\",\"type\":[\"string\", \"null\"]},"
+			+ "{\"name\":\"alias\",\"type\":[\"string\", \"null\"]},"
+			+ "{\"name\":\"atlas\",\"type\":\"boolean\", \"default\": \"false\"},"
+			+ "{\"name\":\"refresh\",\"type\":\"int\", \"default\": \"0\"},"
+			+ "{\"name\":\"name\",\"type\":\"string\"},"
+			+ "{\"name\":\"id\",\"type\":\"string\"}]}");
+	// @formatter:on
+
+	public LayerInfoDTO() {
+		super();
+	}
+
+	@JsonDeserialize(using = CustomRelationDeserializer.class)
+	@JsonSchemaUrl(value = "controller.mapping.CATEGORY")
+	@Valid
+	CategoryDTO parent;
+
+	public CategoryDTO getParent() {
+		return parent;
+	}
+
+	public void setParent(CategoryDTO parent) {
+		this.parent = parent;
+	}
+
+	@Override
+	public Schema getSchema() {
+		return SCHEMA$;
+	}
+
+	@JsonIgnore
+	@Override
+	public Object get(int field) {
+		switch (field) {
+		case 0:
+			return getThemeInspire();
+		case 1:
+			return getLatLonBoundsImage();
+		case 2:
+			return getProtocols();
+		case 3:
+			return getDescription();
+		case 4:
+			return getAlias();
+		case 5:
+			return getAtlas();
+		case 6:
+			return getRefresh();
+		case 7:
+			return getName();
+		case 8:
+			return getId();
+		default:
+			throw new org.apache.avro.AvroRuntimeException("Bad index");
+		}
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@JsonIgnore
+	@Override
+	public void put(int field, Object value) {
+		switch (field) {
+		case 0:
+			setThemeInspire(value != null ? (ThemeInspireDTO) value : null);
+			break;
+		case 1:
+			setLatLonBoundsImage(value != null ? (LatLonBoundingBoxDTO) value : null);
+			break;
+		case 2:
+			setProtocols(value != null ? (java.util.List) value : null);
+			break;
+		case 3:
+			setDescription(value != null ? value.toString() : null);
+			break;
+		case 4:
+			setAlias(value != null ? value.toString() : null);
+			break;
+		case 5:
+			setAtlas((Boolean) value);
+			break;
+		case 6:
+			setRefresh((int) value);
+			break;
+		case 7:
+			setName(value.toString());
+			break;
+		case 8:
+			setId(value.toString());
+			break;
+		default:
+			throw new org.apache.avro.AvroRuntimeException("Bad index");
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LayerInfoDTO other = (LayerInfoDTO) obj;
+		if (parent == null) {
+			if (other.parent != null)
+				return false;
+		} else if (!parent.equals(other.parent))
+			return false;
+		return true;
+	}
+}
---
+++ atlas-lib/src/test/java/es/redmic/atlaslib/unit/dto/layerinfo/LayerInfoCheckDTOValidationTest.java
@@ -0,0 +1,130 @@
+package es.redmic.atlaslib.unit.dto.layerinfo;
+
+import java.util.ArrayList;
+
+/*-
+ * #%L
+ * Atlas-lib
+ * %%
+ * Copyright (C) 2019 REDMIC Project / Server
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.redmic.atlaslib.dto.layer.LatLonBoundingBoxDTO;
+import es.redmic.atlaslib.dto.layer.ProtocolDTO;
+import es.redmic.atlaslib.dto.layerinfo.LayerInfoDTO;
+import es.redmic.atlaslib.unit.utils.LayerDataUtil;
+import es.redmic.testutils.dto.DTOBaseTest;
+
+public class LayerInfoCheckDTOValidationTest extends DTOBaseTest<LayerInfoDTO> {
+
+	private static LayerInfoDTO dto;
+
+	@Before
+	public void reset() {
+
+		dto = LayerDataUtil.getLayerInfo();
+	}
+
+	@Test
+	public void validationDTO_NoReturnError_IfDTOIsCorrect() {
+
+		checkDTOHasNoError(dto);
+	}
+
+	@Test
+	public void validationDTO_NoReturnError_IfNameIsNull() {
+
+		dto.setName(null);
+
+		checkDTOHasNoError(dto);
+	}
+
+	@Test
+	public void validationDTO_ReturnNotNullError_IfProtocolIsNull() {
+
+		dto.setProtocols(null);
+
+		checkDTOHasError(dto, NOT_NULL_MESSAGE_TEMPLATE);
+	}
+
+	@Test
+	public void validationDTO_ReturnSizeError_IfProtocolIsEmpty() {
+
+		dto.setProtocols(new ArrayList<ProtocolDTO>());
+
+		checkDTOHasError(dto, SIZE_MESSAGE_TEMPLATE);
+	}
+
+	@Test
+	public void validationDTO_ReturnNotNullError_IfAtlasIsNull() {
+
+		dto.setAtlas(null);
+
+		checkDTOHasError(dto, NOT_NULL_MESSAGE_TEMPLATE);
+	}
+
+	@Test
+	public void validationDTO_ReturnNotNullError_IfRefreshIsNull() {
+
+		dto.setRefresh(null);
+
+		checkDTOHasError(dto, NOT_NULL_MESSAGE_TEMPLATE);
+	}
+
+	@Test
+	public void validationDTO_ReturnSizeError_IfDescriptionExceedsSize() {
+
+		dto.setDescription(generateString(1501));
+
+		checkDTOHasError(dto, SIZE_MESSAGE_TEMPLATE);
+	}
+
+	@Test
+	public void validationDTO_ReturnSizeError_IfAliasExceedsSize() {
+
+		dto.setAlias(generateString(251));
+
+		checkDTOHasError(dto, SIZE_MESSAGE_TEMPLATE);
+	}
+
+	@Test
+	public void validationDTO_ReturnNotNullError_IfThemeInspireFieldIsNull() {
+
+		dto.getThemeInspire().setName(null);
+
+		checkDTOHasError(dto, NOT_NULL_MESSAGE_TEMPLATE);
+	}
+
+	@Test
+	public void validationDTO_ReturnNotNullError_IfLatLonBoundsImageFieldIsNull() {
+
+		LatLonBoundingBoxDTO latLonBoundingBoxDTO = LayerDataUtil.getLatLonBoundingBoxDTO();
+		latLonBoundingBoxDTO.setMinY(null);
+		dto.setLatLonBoundsImage(latLonBoundingBoxDTO);
+		checkDTOHasError(dto, NOT_NULL_MESSAGE_TEMPLATE);
+	}
+
+	@Test
+	public void validationDTO_ReturnNotNullError_IfProtocolFieldIsNull() {
+
+		dto.getProtocols().get(0).setType(null);
+
+		checkDTOHasError(dto, NOT_NULL_MESSAGE_TEMPLATE);
+	}
+}
