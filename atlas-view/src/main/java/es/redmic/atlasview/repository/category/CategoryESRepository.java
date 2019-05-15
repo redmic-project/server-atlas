302150befde4d8d9de8c7358d4dfe24c6227c2e6
Noel Alonso <noel@redmic.es>
2019-05-15 14:22:50 +0100

    Añade componentes para realizar consultas

---
+++ atlas-view/src/main/java/es/redmic/atlasview/repository/category/CategoryESRepository.java
@@ -0,0 +1,140 @@
+package es.redmic.atlasview.repository.category;
+
+/*-
+ * #%L
+ * Atlas-query-endpoint
+ * %%
+ * Copyright (C) 2019 REDMIC Project / Server
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.elasticsearch.action.search.MultiSearchRequest;
+import org.elasticsearch.action.search.MultiSearchResponse;
+import org.elasticsearch.action.search.MultiSearchResponse.Item;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.stereotype.Repository;
+
+import es.redmic.atlasview.model.category.Category;
+import es.redmic.elasticsearchlib.data.repository.RWDataESRepository;
+import es.redmic.exception.common.ExceptionType;
+import es.redmic.exception.elasticsearch.ESQueryException;
+import es.redmic.models.es.common.dto.EventApplicationResult;
+import es.redmic.models.es.common.query.dto.SimpleQueryDTO;
+import es.redmic.viewlib.data.repository.IDataRepository;
+
+@Repository
+public class CategoryESRepository extends RWDataESRepository<Category, SimpleQueryDTO>
+		implements IDataRepository<Category, SimpleQueryDTO> {
+
+	private static String[] INDEX = { "category" };
+	private static String TYPE = "_doc";
+
+	// @formatter:off
+	
+	private final String ID_PROPERTY = "id";
+	// @formatter:on
+
+	public CategoryESRepository() {
+		super(INDEX, TYPE);
+	}
+
+	@Override
+	protected EventApplicationResult checkInsertConstraintsFulfilled(Category modelToIndex) {
+
+		QueryBuilder idTerm = QueryBuilders.termQuery(ID_PROPERTY, modelToIndex.getId());
+
+		MultiSearchRequest request = new MultiSearchRequest();
+
+		SearchSourceBuilder requestBuilderId = new SearchSourceBuilder().query(idTerm).size(1);
+
+		request.add(new SearchRequest().indices(getIndex()).source(requestBuilderId));
+
+		MultiSearchResponse sr;
+		try {
+			sr = ESProvider.getClient().msearch(request, RequestOptions.DEFAULT);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new ESQueryException();
+		}
+
+		Map<String, String> arguments = new HashMap<>();
+
+		Item[] responses = sr.getResponses();
+
+		if (responses != null && responses[0].getResponse().getHits().getTotalHits() > 0) {
+			arguments.put(ID_PROPERTY, modelToIndex.getId());
+		}
+
+		if (arguments.size() > 0) {
+			return new EventApplicationResult(ExceptionType.ES_INSERT_DOCUMENT.toString(), arguments);
+		}
+
+		return new EventApplicationResult(true);
+	}
+
+	@Override
+	protected EventApplicationResult checkUpdateConstraintsFulfilled(Category modelToIndex) {
+		// @formatter:off
+		
+		BoolQueryBuilder idTerm = QueryBuilders.boolQuery()
+				.mustNot(QueryBuilders.termQuery(ID_PROPERTY, modelToIndex.getId()));
+		
+		MultiSearchRequest request = new MultiSearchRequest();
+		
+		SearchSourceBuilder requestBuilderId = new SearchSourceBuilder().query(idTerm).size(1);
+		
+		request.add(new SearchRequest().indices(getIndex()).source(requestBuilderId));
+		
+		// @formatter:on
+
+		MultiSearchResponse sr;
+		try {
+			sr = ESProvider.getClient().msearch(request, RequestOptions.DEFAULT);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new ESQueryException();
+		}
+
+		Map<String, String> arguments = new HashMap<>();
+
+		Item[] responses = sr.getResponses();
+
+		if (responses != null && responses[0].getResponse().getHits().getTotalHits() > 0) {
+			arguments.put(ID_PROPERTY, modelToIndex.getId().toString());
+		}
+
+		if (arguments.size() > 0) {
+			return new EventApplicationResult(ExceptionType.ES_UPDATE_DOCUMENT.toString(), arguments);
+		}
+
+		return new EventApplicationResult(true);
+	}
+
+	@Override
+	protected EventApplicationResult checkDeleteConstraintsFulfilled(String modelToIndexId) {
+
+		// TODO: mirar si tiene hijos
+		return new EventApplicationResult(true);
+	}
+}
