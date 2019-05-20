24dae97e09ea8d8bd6f5e148d078e038eb266eeb
Noel Alonso <noel@redmic.es>
2019-05-20 11:17:54 +0100

    Añade clase para almacenar config de capas

---
+++ atlas-view/src/main/java/es/redmic/atlasview/model/layer/Dimension.java
@@ -0,0 +1,34 @@
+package es.redmic.atlasview.model.layer;
+
+public class Dimension {
+
+	private String name;
+
+	private String units;
+
+	private String defaultValue;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUnits() {
+		return units;
+	}
+
+	public void setUnits(String units) {
+		this.units = units;
+	}
+
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+
+	public void setDefaultValue(String defaultValue) {
+		this.defaultValue = defaultValue;
+	}
+}
