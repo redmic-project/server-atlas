94dc1741990359cea3a0bc68fcd6a4bdc80f97d2
Noel Alonso <noel@redmic.es>
2019-05-23 11:32:49 +0100

    Añade nuevo campo en el modelo

--- atlas-view/src/main/java/es/redmic/atlasview/model/layer/Dimension.java
+++ atlas-view/src/main/java/es/redmic/atlasview/model/layer/Dimension.java
@@ -1,10 +1,32 @@
 package es.redmic.atlasview.model.layer;
 
+/*-
+ * #%L
+ * Atlas-query-endpoint
+ * %%
+ * Copyright (C) 2019 REDMIC Project / Server
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 public class Dimension {
 
 	private String name;
 
 	private String units;
+
+	private String unitSymbol;
 
 	private String defaultValue;
 
@@ -22,6 +44,14 @@ public class Dimension {
 
 	public void setUnits(String units) {
 		this.units = units;
+	}
+
+	public String getUnitSymbol() {
+		return unitSymbol;
+	}
+
+	public void setUnitSymbol(String unitSymbol) {
+		this.unitSymbol = unitSymbol;
 	}
 
 	public String getDefaultValue() {
