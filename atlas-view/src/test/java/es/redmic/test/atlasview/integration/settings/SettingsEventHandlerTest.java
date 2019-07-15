b2d5799f58c6018ab6c7902da148a2cd01fa5133
Noel Alonso <noel@redmic.es>
2019-07-15 12:40:07 +0100

    Añade tests para la vista de settings

---
+++ atlas-view/src/test/java/es/redmic/test/atlasview/integration/settings/SettingsEventHandlerTest.java
@@ -0,0 +1,55 @@
+package es.redmic.test.atlasview.integration.settings;
+
+/*-
+ * #%L
+ * Atlas-query-endpoint
+ * %%
+ * Copyright (C) 2019 REDMIC Project / Server
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import javax.annotation.PostConstruct;
+
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import es.redmic.atlasview.AtlasViewApplication;
+import es.redmic.viewlib.usersettings.common.SettingsEventHandlerBase;
+
+@SpringBootTest(classes = { AtlasViewApplication.class })
+@RunWith(SpringJUnit4ClassRunner.class)
+@KafkaListener(topics = "${broker.topic.settings}", groupId = "test")
+@TestPropertySource(properties = { "schema.registry.port=19004" })
+@DirtiesContext
+@ActiveProfiles("test")
+public class SettingsEventHandlerTest extends SettingsEventHandlerBase {
+
+	@ClassRule
+	public static EmbeddedKafkaRule embeddedKafka = new EmbeddedKafkaRule(1);
+
+	@PostConstruct
+	public void CreateCategoryFromRestTestPostConstruct() throws Exception {
+
+		createSchemaRegistryRestApp(embeddedKafka.getEmbeddedKafka().getZookeeperConnectionString(),
+				embeddedKafka.getEmbeddedKafka().getBrokersAsString());
+	}
+}
