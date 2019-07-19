3f583bce72e0cfed32471b6e574af0a62336f78e
Noel Alonso <noel@redmic.es>
2019-07-19 08:50:30 +0100

    Añade ejecución de tests integración de settings
    Los tests están definidos en la librería, pero al ser de integración,
    debe aplicarse un contexto y ejecutarse en la unidad funcional

---
+++ atlas-commands/src/test/java/es/redmic/test/atlascommands/integration/usersettings/SettingsCommandHandlerTests.java
@@ -0,0 +1,58 @@
+package es.redmic.test.atlascommands.integration.usersettings;
+
+import javax.annotation.PostConstruct;
+
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import es.redmic.atlascommands.AtlasCommandsApplication;
+import es.redmic.commandslib.usersettings.common.SettingsCommandHandlerBase;
+import es.redmic.test.atlascommands.integration.KafkaEmbeddedConfig;
+
+/*-
+ * #%L
+ * Atlas-management
+ * %%
+ * Copyright (C) 2019 REDMIC Project / Server
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = { AtlasCommandsApplication.class })
+@ActiveProfiles("test")
+@DirtiesContext
+@KafkaListener(topics = "${broker.topic.settings}", groupId = "SettingsCommandHandlerTests")
+@TestPropertySource(properties = { "spring.kafka.consumer.group-id=SettingsCommandHandler",
+		"schema.registry.port=19199" })
+public class SettingsCommandHandlerTests extends SettingsCommandHandlerBase {
+
+	@ClassRule
+	public static EmbeddedKafkaRule embeddedKafka = new EmbeddedKafkaRule(KafkaEmbeddedConfig.NUM_BROKERS, true,
+			KafkaEmbeddedConfig.PARTITIONS_PER_TOPIC, KafkaEmbeddedConfig.TOPICS_NAME);
+
+	@PostConstruct
+	public void CategoryCommandHandlerTestPostConstruct() throws Exception {
+
+		createSchemaRegistryRestApp(embeddedKafka.getEmbeddedKafka().getZookeeperConnectionString(),
+				embeddedKafka.getEmbeddedKafka().getBrokersAsString());
+	}
+}
