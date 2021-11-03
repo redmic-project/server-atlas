44b1279b8fa7ef0c3f0bd8af613d7389d4a562a4
Noel <noelalonso1986@gmail.com>
2021-11-03 14:49:10 +0000

    Adapta modelo y tests para nuevos campos

--- atlas-view/src/main/java/es/redmic/atlasview/model/layer/Download.java
+++ atlas-view/src/main/java/es/redmic/atlasview/model/layer/Download.java
@@ -4,14 +4,14 @@ package es.redmic.atlasview.model.layer;
  * #%L
  * Atlas-query-endpoint
  * %%
- * Copyright (C) 2019 REDMIC Project / Server
+ * Copyright (C) 2019 - 2021 REDMIC Project / Server
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,21 +20,21 @@ package es.redmic.atlasview.model.layer;
  * #L%
  */
 
-public class Protocol {
+public class Download {
 
-	private String type;
+	private String name;
 
 	private String url;
 
-	public Protocol() {
+	public Download() {
 	}
 
-	public String getType() {
-		return type;
+	public String getName() {
+		return this.name;
 	}
 
-	public void setType(String type) {
-		this.type = type;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getUrl() {
